d5476b8cc136c2117799130947f88f01e5564d67
PatoBeltran <pbelsal@gmail.com>
2015-02-07 19:52:13 -0600

    Add hit creation endpoint

--- app/controllers/api/v1/hits_controller.rb
+++ app/controllers/api/v1/hits_controller.rb
@@ -11,4 +11,20 @@ class Api::V1::HitsController < API::V1::BaseController
       head 404
     end
   end
+
+  def create
+    hit = Hit.new(hit_params)
+
+    if hit.save
+      render json: hit, serializer: HitSerializer, status: 200
+    else
+      render json: { hit: { errors: hit.errors.full_messages } }, status: 422
+    end
+  end
+
+  private
+
+  def hit_params
+    params.require(:hit).permit(:latitude, :longitud, :report, :user_id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # Routes without api subdomain for staging env
   namespace :api, path: '/api' do
     api_version(:module => "V1", :header => {:name => "Accept", :value => "application/vnd.copapi.com+json; version=1"}) do
-      resources :hits, only: [:index, :show]
+      resources :hits, only: [:index, :show, :create]
     end
   end
 
