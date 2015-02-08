6c398d6ec4ae33171b11aacc4905aa8c0faf5042
PatoBeltran <pbelsal@gmail.com>
2015-02-07 19:29:28 -0600

    Add hit show api endpoint

--- app/controllers/api/v1/hits_controller.rb
+++ app/controllers/api/v1/hits_controller.rb
@@ -2,4 +2,13 @@ class Api::V1::HitsController < API::V1::BaseController
   def index
     render json: Hit.all, each_serializer: HitSerializer, status: 200
   end
+
+  def show
+    hit = Hit.find(params[:id])
+    if (hit)
+      render json: hit, serializer: HitSerializer, status: 200
+    else
+      head 404
+    end
+  end
 end
